5d392781025b96d13ce23d37246e129c5fb4cc7d
McKenna Penley <mckennapenley@gmail.com>
2021-11-28 20:43:14 -0500

    looks for existing question response and updates, else creates new response

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -1,13 +1,24 @@
 class Api::ResponsesController < ApplicationController
   def create
+
     game = Game.find(params[:game_id])
-    teams = game.teams
+
     question_id = game.questions.find_by(order:params[:order]).id
-    Response.create(answered_correctly:params[:answered_correctly], team_id:params[:team_id],question_id:question_id)
-    
+
+    exisiting_response = Response.find_by(team_id:params[:team_id], question_id:question_id)
+
+    teams = game.teams
+
+   
+
+    if exisiting_response && (exisiting_response.answered_correctly != params[:answered_correctly])
+      exisiting_response.update!(answered_correctly:params[:answered_correctly])
+    elsif !exisiting_response 
+      Response.create(answered_correctly:params[:answered_correctly], team_id:params[:team_id],question_id:question_id)
+    end
 
     render json: {  
-      teams: teams.map { |team| TeamSerializer.new(team) }
+    teams: teams.map { |team| TeamSerializer.new(team) }
     }
   end
 end
