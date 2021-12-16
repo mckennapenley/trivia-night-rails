5f6844722c60c9ad50a18f7f9e24683d5348141b
McKenna Penley <mckennapenley@gmail.com>
2021-12-16 16:45:47 -0500

    Updates routes to use get instead of post

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
  
   namespace :api, defaults: { format: 'json' } do
     post 'games/start_game', to: 'games#start_game'
-    post 'games/:game_id/questions/:order', to: 'questions#show'
-    post 'games/:game_id/teams', to: 'teams#index'
+    get 'games/:game_id/questions/:order', to: 'questions#show'
+    get 'games/:game_id/teams', to: 'teams#index'
     post 'games/:game_id/questions/:order/create', to: 'responses#create'
     post 'games/:game_id/end_game', to: 'games#end_game'
   end
