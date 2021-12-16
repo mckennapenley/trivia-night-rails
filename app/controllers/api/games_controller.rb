b0c61650ee60e9e3777c7fc527959f6f535d57d7
McKenna Penley <mckennapenley@gmail.com>
2021-12-16 16:45:22 -0500

    Refactors to controller to use private methods

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,12 +1,9 @@
 class Api::GamesController < ApplicationController
+  before_action :validate_params, only: :start_game
   # POST /api/games
   def start_game
-    game = Game.create(question_quantity: params[:game][:question_qty].to_i)
-    questions = QuestionService.get_questions(params[:game][:question_qty].to_i, params[:game][:difficulty])
-
-    
     questions.each_with_index do |question, index| 
-      Question.create(
+      Question.create!(
         points: 100, 
         prompt: question["question"], 
         answer: question["correct_answer"], 
@@ -15,10 +12,9 @@ class Api::GamesController < ApplicationController
       )
     end
 
-    teams = (params[:game].except(:difficulty, :question_qty)).values
-    teams.each do |team|
-      Team.create(
-        name: team,
+    team_names.each do |team_name|
+      Team.create!(
+        name: team_name,
         game_id: game.id
       )
     end
@@ -29,23 +25,37 @@ class Api::GamesController < ApplicationController
  
   def end_game
     game = Game.find(params[:game_id])
-    teams = game.teams
 
     render json: {  
-    teams: teams.map { |team| TeamSerializer.new(team) }
+      teams: game.teams.map { |team| TeamSerializer.new(team) }
     }
 
   end
+
+  private
+
+  def game
+    @game ||= Game.create!(question_quantity: question_quantity)
+  end
+
+  def question_quantity
+    @question_quantity ||= params[:game][:question_qty].to_i
+  end
+
+  def game_difficulty
+    @game_difficulty ||= params[:game][:difficulty]
+  end
+
+  def questions
+    @questions ||= QuestionService.get_questions(question_quantity, game_difficulty)
+  end
+
+  def team_names
+    @team_names ||= params[:game].except(:difficulty, :question_qty).values
+  end
+
+  def validate_params
+    raise if team_names.empty? || question_quantity < 1 || game_difficulty.blank?
+  end
 end
 
-
-# def next_question
-#   current_question = Question.find(22)
-
-#   render json { question: current_question.next_question}
-# end
-
-# # Question Model
-# def next_question
-#   Question.find_by(game_id: self.game_id, order: self.order + 1)
-# end
